7f165c62092a449c7b1b01bc887f720848cb74e2
ariflateefshah <arif@ideas2it.com>
2019-05-25 13:00:19 +0530

    events with and without args

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var myView =  new Vue({
         el:"#myApp",
         data:{
-            name:"Facebook Link",
-            url:"https://www.facebook.com",
-            title:"This is facebook link",
-            imageUrl:'./images/image1.png',
-            isActive:true,
-            name:'Arif',
-            email:'arif@shah.com',
-            htmlContent:"<p><b>This content is comming from Backend<b></p>"
+            counter: 0
         },
         methods:{
+            increment: function(){
+               
+                this.counter ++;
+            },
+            decrement: function(num){
+                 this.counter -= num;
+            }
         }
     });
