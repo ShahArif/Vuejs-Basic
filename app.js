29cd1f69167fc62191d3cdf28a3387d0a70ae3c5
ariflateefshah <arif@ideas2it.com>
2019-06-12 13:01:16 +0530

    Basic understanding of Computed property

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var myView =  new Vue({
         el:"#myApp",
         data:{
-            isUser:false,
-            dashboard :true,
-            login:false,
+            a:0,
+            b:0,
+            age:5,
         },
         methods:{
+            // addToA: function(){
+            //     console.log('AddTo A Is Running');
+            //     return this.age + this.a
+            // },
+            // addToB: function(){
+            //     console.log('AddTo B Is Running');
+            //     return this.age + this.b
+            // }
+        },
+        // Copy same code which was in methods and paste it in computed
+        computed:{
+            addToA: function(){
+                console.log('AddTo A Is Running');
+                return this.age + this.a
+            },
+            addToB: function(){
+                console.log('AddTo B Is Running');
+                return this.age + this.b
+            }
         }
     });
 
