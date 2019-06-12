4c0f35a235041604174bfd38b099723b3010e7f3
ariflateefshah <arif@ideas2it.com>
2019-06-12 12:45:38 +0530

    basic structure for Vuejs

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 var myView =  new Vue({
         el:"#myApp",
         data:{
-            isbgGreen:true,
-            isbgBlue :false,
-            isActive:false,
+            isUser:false,
+            dashboard :true,
+            login:false,
         },
         methods:{
-            chnageColorToGreen: function(event){
-                alert('heylo');
-               this.isbgBlue = false;
-               this.isbgGreen = true;
-            },
-            chnageColorToBlue: function(event){
-               this.isbgGreen = false;
-               this.isbgBlue = true;
-            },
-            toggleColor : function(){
-                alert('hey');
-                this.isActive = !this.isActive;
-            }
         }
     });
 
